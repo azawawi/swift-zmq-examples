0fd55373ece3c1a400460dff7cca51401e35d0e8
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-10-13 10:03:30 +0300

    Bump dependency on swift-zmq (aka ZMQ)

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@ import PackageDescription
 let package = Package(
     name: "ZMQExamples",
     dependencies: [
-        .Package(url: "https://github.com/azawawi/swift-zmq.git", majorVersion: 0, minor: 2),
+        .Package(
+            url          : "https://github.com/azawawi/swift-zmq.git",
+            majorVersion : 0,
+            minor        : 4
+        )
     ]
 )
